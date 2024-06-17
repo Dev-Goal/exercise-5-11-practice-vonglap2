3105c9e9c7599982c921a15e8691a831f80d5d5c
Admin <lmht721@gmail.com>
2024-06-17 15:13:11 +0700

    update

--- js/bt4.js
+++ js/bt4.js
@@ -1,14 +1,14 @@
-let a = " "
-        for (let i=0;i<=10;i++){
-            for (let j = 0;j<=50;j++){
-                if (i == 0 || i == 10){
-                    document.write("<a style='color: red'>*<a>")
-                }else{
-                    document.write("<a style='color: white'>*<a>")
-                }
-                if (j == 0 || j== 50) {
-                    document.write("<a style='color: red'>*<a>")
-                }
-            }
-            document.write("<br>")
-        }
+let a = " ";
+for (let i = 0; i <= 10; i++) {
+  for (let j = 0; j <= 50; j++) {
+    if (i == 0 || i == 10) {
+      document.write("<a style='color: black'>*<a>");
+    } else {
+      document.write("<a style='color: white'>*<a>");
+    }
+    if (j == 0 || j == 50) {
+      document.write("<a style='color: black'>*<a>");
+    }
+  }
+  document.write("<br>");
+}
